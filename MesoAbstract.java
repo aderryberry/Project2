c9d29222c5e332716f3c889d71aced331f74b2cc
Andrew Derryberry <andrew.r.derryberry-1@ou.edu>
2019-10-10 16:16:08 -0500

    Project 2 upload 9

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -7,6 +7,7 @@ public abstract class MesoAbstract
 	//Abstract method calAverage to return integer array.
 	abstract int[] calAverage();
 
+	// Abstract method for the letterAverage returning char
 	abstract char letterAverage();
 	
 }
