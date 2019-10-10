399e0db2d2191411c6798304080e53f07218589e
Andrew Derryberry <andrew.r.derryberry-1@ou.edu>
2019-10-10 16:08:32 -0500

    Project 2 upload 6

--- LetterAvg.java
+++ LetterAvg.java
@@ -12,8 +12,10 @@ public class LetterAvg {
 	private ArrayList<String> sameAvgLetter = new ArrayList<String>();
 	private ArrayList<String> stationList = new ArrayList<String>();
 
+	
+	// Constructor for letter average. Calls a method to create the station list
 	public LetterAvg(char letterAvg) {
-		// TODO Auto-generated constructor stub
+		
 		this.letterAvg = letterAvg;
 		try {
 			createStationList();
@@ -24,17 +26,20 @@ public class LetterAvg {
 
 	}
 
+	// Method to calculate the number of stations with the same first letter as the average letter
 	public int numberOfStationWithLetterAvg() throws IOException {
 		// TODO Auto-generated method stub
 		int numStations = 0;
 		int stationCounter = 0;
 
+		//cycle through all the stations and find the number of stations that have the same letter as the avg
 		for (int i = 0; i < stationList.size(); i++) {
 			if (letterAvg == stationList.get(i).charAt(0)) {
 				numStations++;
 			}
 		}
 
+		//cycle through the stations again and add the stations that share the same avg letter to an array list
 		for (int i = 0; i < stationList.size(); i++) {
 			if (letterAvg == stationList.get(i).charAt(0)) {
 				sameAvgLetter.add(stationCounter, stationList.get(i));
@@ -49,17 +54,22 @@ public class LetterAvg {
 	public void createStationList() throws IOException {
 		int numberOfStations=0;
 		
+		// read the number of lines in the provided txt document
 		BufferedReader br=new BufferedReader(new FileReader("Mesonet.txt"));
 		while (br.readLine()!=null) {
 			numberOfStations++;
 		}
+		// subtract the number of lines for the given txt document that just describe the txt
+		// the lines without the stations in them
 		numberOfStations=numberOfStations-linesDescribingTXTdoc;
 		
+		// cycle through the station list and add the stations to the array
 		BufferedReader reader=new BufferedReader(new FileReader("Mesonet.txt"));
+		// cycle through the first lines that do not contain stations
 		for (int i=0; i<linesDescribingTXTdoc;i++) {
 			reader.readLine();
 		}
-		
+		// read in the stations in the txt document and only take the parts with the station name
 		for (int i=0; i<numberOfStations; i++) {
 			stationList.add(reader.readLine().substring(beginIndex, endIndex));
 
